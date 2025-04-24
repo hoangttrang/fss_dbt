c8bbf32a87d86eaae028da47769a01e12cb8023a
hoangttrang <trang.t.hoang.1@vanderbilt.edu>
2025-04-24 15:11:35 -0500

    edit bugs from the migrations

--- models/analytics/mbr_report_2024.sql
+++ models/analytics/mbr_report_2024.sql
@@ -9,20 +9,49 @@ WITH monthly_events AS (
     SELECT * FROM {{ ref('int_motive_vehicle_group_map_rs') }}
 )
 
-, safety_scores_ranked AS (
-    SELECT * FROM {{ ref('int_motive_safety_scores_ranked') }}
+, safety_scores_metrics AS (
+    SELECT * FROM {{ ref('int_motive_safety_scores_metrics') }}
 )
 
-, final_events_per_vehicle AS (
-    SELECT * FROM {{ ref('int_motive_events_p_vehicles_ranked') }}
+, final_events_per_vehicle_metrics AS (
+    SELECT * FROM {{ ref('int_motive_events_p_vehicles_metrics') }}
 )
 
 , rank_trips AS ( 
     SELECT * FROM {{ ref('int_motive_rank_trips') }}
 )
 
+, dvir_metrics AS ( 
+    SELECT * FROM {{ ref('int_motive_dvir_metrics') }}
+)
+
 , dvir_rank AS ( 
-    SELECT * FROM {{ ref('int_motive_dvir_rank') }}
+
+    SELECT *,
+        RANK() OVER (PARTITION BY "Metric" ORDER BY "{{ month_str }}" DESC) AS "Company Rank",
+        RANK() OVER (PARTITION BY "Metric", "Region" ORDER BY "{{ month_str }}" DESC) AS "Region Rank"
+    FROM dvir_metrics
+    WHERE "Location" IS NOT NULL
+    ORDER BY "Metric", "Region", "Location"
+
+)
+
+, final_events_per_vehicle AS (
+    SELECT *
+        , RANK() OVER (ORDER BY "{{month_str}}" ASC) AS "Company Rank" 
+        , RANK() OVER (PARTITION BY "Region" ORDER BY "{{month_str}}" ASC) AS "Region Rank"
+    FROM final_events_per_vehicle_metrics
+    WHERE "Location" IS NOT NULL
+)
+
+, safety_scores_ranked AS (
+
+    SELECT *,
+        RANK() OVER (ORDER BY "{{month_str}}" DESC) AS "Company Rank",
+        RANK() OVER (PARTITION BY "Region" ORDER BY "{{month_str}}" DESC) AS "Region Rank"
+    FROM safety_scores_metrics
+    WHERE "Location" IS NOT NULL
+    
 )
 
 , ranked_events_moved_to_uncoachable AS (
