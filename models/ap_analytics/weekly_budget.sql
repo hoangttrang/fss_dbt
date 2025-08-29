d213a4ec8d98a7d02047f4d46103a7c847a23ea0
hoangttrang <trang.t.hoang.1@vanderbilt.edu>
2025-08-29 11:09:17 -0500

    add weekly and daily budget

---
+++ models/ap_analytics/weekly_budget.sql
@@ -0,0 +1,70 @@
+WITH date_map AS ( 
+    SELECT DISTINCT * FROM {{ ref('int_dims_week') }}
+)
+
+, sage_budget_item AS ( 
+    SELECT * FROM {{ ref('int_sage_monthly_budget') }}
+)
+
+, list_of_acct_department_date AS (
+    SELECT
+        *
+    FROM (SELECT DISTINCT acct_no, dept_no, class_id, dep_title, class_name
+        FROM sage_budget_item)
+    CROSS JOIN (SELECT * FROM date_map)
+)
+
+, add_budget AS (SELECT 
+    list.* 
+    ,  (
+          (make_date(EXTRACT(YEAR FROM list.week_start_date)::INT,
+                     EXTRACT(MONTH FROM list.week_start_date)::INT, 1) + INTERVAL '1 month')::date
+          - make_date(EXTRACT(YEAR FROM list.week_start_date)::INT,
+                      EXTRACT(MONTH FROM list.week_start_date)::INT, 1)
+        )::INT AS start_month_days
+    ,  (
+          (make_date(EXTRACT(YEAR FROM list.week_end_date)::INT,
+                     EXTRACT(MONTH FROM list.week_end_date)::INT, 1) + INTERVAL '1 month')::date
+          - make_date(EXTRACT(YEAR FROM list.week_end_date)::INT,
+                      EXTRACT(MONTH FROM list.week_end_date)::INT, 1)
+        )::INT AS end_month_days
+    , budget_month1.total_amnt AS budget_month_start
+    , budget_month2.total_amnt AS budget_month_end
+FROM list_of_acct_department_date AS list
+LEFT JOIN sage_budget_item AS budget_month1 
+ON budget_month1.acct_no = list.acct_no
+    AND budget_month1.dept_no = list.dept_no
+    AND budget_month1.class_id = list.class_id
+    AND budget_month1.report_year = list.year_start_num
+    AND budget_month1.report_month = list.month_start_num
+LEFT JOIN sage_budget_item AS budget_month2
+ON budget_month2.acct_no = list.acct_no
+    AND budget_month2.dept_no = list.dept_no
+    AND budget_month2.class_id = list.class_id
+    AND budget_month2.report_year = list.year_end_num
+    AND budget_month2.report_month = list.month_end_num
+WHERE budget_month1.total_amnt IS NOT NULL AND budget_month2.total_amnt IS NOT NULL) 
+
+, calculate_weekly_budget AS ( 
+    SELECT 
+       acct_no 
+       , dept_no 
+       , dep_title
+       , class_id
+       , class_name
+       , week_start_date
+       , week_end_date
+       , budget_month_start
+       , budget_month_end
+       , days_in_month_start_in_week
+       , days_in_month_end_in_week
+       , start_month_days
+       , end_month_days
+       , ROUND((
+            budget_month_start::numeric / start_month_days * days_in_month_start_in_week
+                + budget_month_end::numeric / end_month_days * days_in_month_end_in_week
+        ), 2 ) AS weekly_budget
+    FROM add_budget
+)   
+
+select * from calculate_weekly_budget
---
+++ models/intermediate/int_dims_week.sql
@@ -0,0 +1,87 @@
+{{ config(materialized='table') }}
+
+/* 
+
+This table generates the weekly boundaries for a date spine that it is suitable for Financial Reporting, especially in this case it use to 
+break down Monthly Budget into weekly Budget 
+
+*/
+WITH date_spine AS (
+
+    -- Generate one row per day from some min to max date
+    SELECT
+        date_day::DATE AS date_day
+    FROM generate_series(
+        '2023-12-30'::DATE,               
+        current_date + interval '1 year', 
+        interval '1 day'
+    ) AS t(date_day)
+
+),
+
+week_boundaries AS (
+
+    SELECT
+        date_day,
+        -- Shift back to Monday (ISO: dow=1 means Monday, dow=0 means Sunday)
+        date_day - ((EXTRACT(DOW FROM date_day)::INT + 6) % 7) AS week_start_date,
+        -- Always week_start_date + 6
+        date_day - ((EXTRACT(DOW FROM date_day)::INT + 6) % 7) + 6 AS week_end_date
+    FROM date_spine
+)
+
+, month_spine AS (
+    SELECT 
+        (DATE_TRUNC('month', d))::DATE AS month_start
+    FROM GENERATE_SERIES(
+        '2024-01-01'::DATE,
+        '2026-12-01'::DATE,
+        INTERVAL '1 month'
+    ) AS gs(d)
+)
+
+, month_year AS (
+    SELECT
+        EXTRACT(YEAR  FROM month_start)::INT  AS year,
+        EXTRACT(MONTH FROM month_start)::INT  AS month,
+        EXTRACT(DAY   FROM (month_start + INTERVAL '1 month - 1 day'))::INT AS days_in_month
+    FROM month_spine
+    ORDER BY year, month
+)
+
+, week_boundary AS (
+    SELECT 
+        DISTINCT
+        week_start_date,
+        week_end_date,
+        EXTRACT(MONTH FROM week_start_date)::INT AS month_start_num,  -- e.g., 1 for Jan
+        EXTRACT(YEAR  FROM week_start_date)::INT AS year_start_num,   -- e.g., 2024
+        EXTRACT(MONTH FROM week_end_date)::INT   AS month_end_num,     -- e.g., 2 for Feb
+        EXTRACT(YEAR  FROM week_end_date)::INT   AS year_end_num,       -- e.g., 2024
+        /* Days of the week that lie in the start month */
+        GREATEST(
+            0,
+            LEAST(
+                week_end_date,
+                (DATE_TRUNC('month', week_start_date) + INTERVAL '1 month - 1 day')::DATE
+            ) - week_start_date + 1
+        )::INT AS days_in_month_start_in_week,
+
+        /* Days of the week that lie in the end month (0 if same month) */
+        CASE
+            WHEN DATE_TRUNC('month', week_start_date) = DATE_TRUNC('month', week_end_date)
+                THEN 0
+            ELSE (7 - GREATEST(
+                        0,
+                        LEAST(
+                            week_end_date,
+                            (DATE_TRUNC('month', week_start_date) + INTERVAL '1 month - 1 day')::DATE
+                        ) - week_start_date + 1
+                    ))
+        END::INT AS days_in_month_end_in_week
+
+    FROM week_boundaries
+    ORDER BY week_start_date
+)
+
+SELECT * FROM week_boundary
